1eb9e9580d6121688f9ca10d82518968a7ab1e7b
Pablo Alan <pabloxt14@gmail.com>
2025-12-19 16:54:29 -0300

    style: add icons lib

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import { PiSun, PiMoonStars } from "react-icons/pi"
+import {
+  IoLogoTwitter,
+  IoLogoTwitch,
+  IoLogoGithub,
+  IoLogoFacebook,
+  IoLogoInstagram,
+  IoLogoYoutube,
+  IoLogoLinkedin,
+  IoLogoWhatsapp,
+  IoLogoTiktok,
+} from "react-icons/io5"
+
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-center gap-2 bg-background">
@@ -7,6 +20,21 @@ export default function Home() {
 
       <p className="text-body-md">Text 1</p>
       <p className="text-body-sm">Text 2</p>
+
+      <div className="flex gap-2">
+        <PiSun className="size-6" />
+        <PiMoonStars className="size-6" />
+
+        <IoLogoTwitter className="size-6" />
+        <IoLogoTwitch className="size-6" />
+        <IoLogoGithub className="size-6" />
+        <IoLogoFacebook className="size-6" />
+        <IoLogoInstagram className="size-6" />
+        <IoLogoYoutube className="size-6" />
+        <IoLogoLinkedin className="size-6" />
+        <IoLogoWhatsapp className="size-6" />
+        <IoLogoTiktok className="size-6" />
+      </div>
     </div>
   )
 }
