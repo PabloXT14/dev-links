3904dd70a109d8e8bd344d76ddb31851351fb89f
Pablo Alan <pabloxt14@gmail.com>
2026-01-23 16:57:56 -0300

    refactor: edit avatar quality

--- src/components/avatar/index.tsx
+++ src/components/avatar/index.tsx
@@ -5,6 +5,7 @@ type AvatarProps = PrismicNextImageProps
 export const Avatar = async (props: AvatarProps) => (
   <PrismicNextImage
     className="size-28 rounded-full border-2 border-stroke bg-linear-to-r from-fuchsia-500 via-violet-500 to-cyan-500 bg-origin-border object-cover object-center"
+    quality={80}
     {...props}
   />
 )
